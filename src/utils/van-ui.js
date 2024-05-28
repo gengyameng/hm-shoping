b3978e20db5ece86c0b2fe1efc7e450e7a709167
Eirene Jackson <33662520+gengyameng@users.noreply.github.com>
2024-05-28 14:16:53 +0800

    组件

--- src/utils/van-ui.js
+++ src/utils/van-ui.js
@@ -1,6 +1,6 @@
 // 按需导入
 import Vue from 'vue'
-import { Button, Rate, Tabbar, TabbarItem, NavBar, Toast } from 'vant'
+import { Button, Rate, Tabbar, TabbarItem, NavBar, Toast, Search, Swipe, SwipeItem, Grid, GridItem, Icon, ActionSheet, Dialog, Checkbox, CheckboxGroup, Tab, Tabs } from 'vant'
 
 Vue.use(Button)
 Vue.use(Rate)
@@ -8,3 +8,15 @@ Vue.use(Tabbar)
 Vue.use(TabbarItem)
 Vue.use(NavBar)
 Vue.use(Toast)
+Vue.use(Search)
+Vue.use(Swipe)
+Vue.use(SwipeItem)
+Vue.use(Grid)
+Vue.use(GridItem)
+Vue.use(Icon)
+Vue.use(ActionSheet)
+Vue.use(Dialog)
+Vue.use(Checkbox)
+Vue.use(CheckboxGroup)
+Vue.use(Tab)
+Vue.use(Tabs)
